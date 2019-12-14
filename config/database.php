d6a99c123820c841405abd711f3d1ba1c271f8ba
JulioRValverde <julio.valverdem@autonoma.edu.co>
2019-12-13 23:24:57 -0500

    Base de datos 4

--- config/database.php
+++ config/database.php
@@ -47,8 +47,8 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8',
-            'collation' => 'utf8',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'strict' => true,
             'engine' => null,
@@ -56,6 +56,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
+
             'host' => env('DB_HOST', 'ec2-107-21-235-87.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'demaeo6afdpo8q'),
