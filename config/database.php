9a6bc36da3e7d9ef0485e417b2e04467efae84df
JulioRValverde <julio.valverdem@autonoma.edu.co>
2019-12-13 23:02:51 -0500

    Base de datos 2

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8',
-            'collation' => 'utf8mb4_unicode_ci',
+            'collation' => 'utf8',
             'prefix' => '',
             'strict' => true,
             'engine' => null,
