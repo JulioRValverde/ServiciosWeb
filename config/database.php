0b6b3ab20134d83059d2e3e1b5dd746f6499fb2e
JulioRValverde <julio.valverdem@autonoma.edu.co>
2019-12-15 20:48:18 -0500

    Primer Commit

--- config/database.php
+++ config/database.php
@@ -56,18 +56,19 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-
-            'host' => env('DB_HOST', 'ec2-107-21-235-87.compute-1.amazonaws.com'),
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'demaeo6afdpo8q'),
-            'username' => env('DB_USERNAME', 'hedzrpbsmkfaxf'),
-            'password' => env('DB_PASSWORD', '26f0ad6ef3ee9c860c6f29def511cbdc5a1a259a9a45f8956d533bb0881c2f3f'),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
             'prefix' => '',
+            'prefix_indexes' => true,
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
+
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
