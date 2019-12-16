6e8c43233843f12f5098d73a3bfda426d95968b2
JulioRValverde <julio.valverdem@autonoma.edu.co>
2019-12-15 22:17:57 -0500

    Arreglo

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,7 @@ use App\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SignUpRequest;
 use App\User;
+use Illuminate\Support\Facades\DB;
 
 
 class AuthController extends Controller
