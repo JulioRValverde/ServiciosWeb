27bd3b5d2e806bd900c63cc7f0c47823389e7d6f
JulioRValverde <julio.valverdem@autonoma.edu.co>
2019-12-15 22:29:29 -0500

    Arreglado

--- app/Http/Requests/SignUpRequest.php
+++ app/Http/Requests/SignUpRequest.php
@@ -27,7 +27,7 @@ class SignUpRequest extends FormRequest
             
             'name'=> 'required',
             'email'=> 'required|email|unique:users',
-            'password'=> 'required|confirmed'
+            'password'=> 'required'
 
         ];
     }
